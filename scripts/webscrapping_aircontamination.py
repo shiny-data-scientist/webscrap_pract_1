3706e97abe9e9b16863d9db6284b98c5b1a949a5
alcort <62378022+alcort@users.noreply.github.com>
2020-03-31 20:01:40 +0200

    Update webscrapping_aircontamination.py

--- scripts/webscrapping_aircontamination.py
+++ scripts/webscrapping_aircontamination.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+from datetime import datetime
 
 def get_page_data_by_city(paths: list):
     """
@@ -33,35 +34,66 @@ def get_page_data_by_city(paths: list):
         actions.perform()
         time.sleep(5)
 
-        # TODO: Se tienen que añadir las demás partículas:
-        #  o3 y no3, por ahora solo está el pm10
-        #  también se tiene que añadir la etiqueta de la calidad del aire.
-        # buscamos partícula pm10 por el xpath
-        pm10 = driver.find_element_by_xpath(
-            '//*[@id="historic-aqidata-inner"]/div[2]/div[2]/center/ul/li[1]'
-        )
-        pm10.click()
+        # Se añaden 3 índices de calidad del aire. PM10, O3 y NO2
+        data = []
+        for x in range(1,4):
+             
+            # buscamos partícula pm10 por el xpath
+            particula = driver.find_element_by_xpath(
+                '//*[@id="historic-aqidata-inner"]/div[2]/div[2]/center/ul/li'+str([x])
+            )
+            particula.click()
 
-        # Comprobamos si existe la página
-        try:
-            table = driver.find_element_by_class_name('squares')
-        except:
-            print(f'Ha habido un error para generar los datos en la página:'
-                  f'path: {current_path}, partícula: pm10')
-            return
+            # Comprobamos si existe la página
+            try:
+                table = driver.find_element_by_class_name('squares')
+            except:
+                print(f'Ha habido un error para generar los datos en la página:'
+                      f'path: {current_path}, partícula: indice')
+                return
 
-        # hacemos una sopa con el código fuente
-        soup = BeautifulSoup(driver.page_source)
+            # hacemos una sopa con el código fuente
+            soup = BeautifulSoup(driver.page_source)
 
-        # Buscamos la tabla que contiene los datos en la sopa HTML
-        table_data = soup.find(
-            id='historic-aqidata-inner'
-        ).find('table').find('tbody')
-        print(table_data)
+            # Buscamos la tabla que contiene los datos en la sopa HTML
+            table_data = soup.find(
+                id='historic-aqidata-inner'
+            ).find('table').find('tbody')
 
-        # TODO: [ ] Código para extraer los datos.
-        #  [ ] Añadir una opción para extraer los datos por fechas (las
-        #  recibimos por parámetros)
+            
+            for row in table_data.find_all('tr'):
+                # cuando el dato está lleno tiene table-row
+                # el year-divider es para el relleno de la tabla
+                if row.get('class') == ['year-divider']:
+                    year = row.text
+                    # Se obtienen solo datos de 2019 y 2020
+                    if year == '2020' or year == '2019': 
+                        if row['style'] == 'display: table-row;' and row.get(
+                        'class') != ['year-divider']:
+                        month = None
+                        for td in row.find_all('td'):
+                            if month is None:
+                                month = row.td.text
+                                print(month)
+                            if td.get('class') == ['squares']:
+                                # comenzamos con el día 1 cada mes
+                                day = 1
+                                for svg in td.find_all('svg'):
+                                    for rect in svg.find_all('text'):
+                                        timestamp = datetime.strptime(
+                                            f'{day}-{month}-{year}',
+                                            '%d-%b-%Y' 
+                                        )
+                                        day += 1
+                                        temp_data = {
+                                            'timestamp': timestamp,
+                                            'particula': rect.text
+                                        }
+                                        data.append(temp_data)
+                                        
+
+            #  [ ] Añadir una opción para extraer los datos por fechas (las
+            #  recibimos por parámetros)
 
     driver.close()
 
